418f1f33bd78bab8558fc490ce7461a3d3899209
KitBS <mail@kitbs.com>
2015-11-02 00:49:32 +0000

    create CT models

--- app/CTInventory.php
+++ app/CTInventory.php
@@ -3,8 +3,54 @@
 namespace Cellar;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CTInventory extends Model
 {
-    //
+    use SoftDeletes;
+
+    public function user()
+    {
+        return $this->belongsTo('Cellar\User');
+    }
+
+    public function availability()
+    {
+        return $this->hasOne('Cellar\CTInventory', 'i_wine', 'i_wine');
+    }
+
+    public function consumed()
+    {
+        return $this->hasOne('Cellar\CTInventory', 'i_wine', 'i_wine');
+    }
+
+    public function list()
+    {
+        return $this->hasOne('Cellar\CTInventory', 'i_wine', 'i_wine');
+    }
+
+    public function notes()
+    {
+        return $this->hasOne('Cellar\CTInventory', 'i_wine', 'i_wine');
+    }
+
+    public function pending()
+    {
+        return $this->hasOne('Cellar\CTInventory', 'i_wine', 'i_wine');
+    }
+
+    public function privateNotes()
+    {
+        return $this->hasOne('Cellar\CTInventory', 'i_wine', 'i_wine');
+    }
+
+    public function proReview()
+    {
+        return $this->hasOne('Cellar\CTInventory', 'i_wine', 'i_wine');
+    }
+
+    public function purchase()
+    {
+        return $this->hasOne('Cellar\CTInventory', 'i_wine', 'i_wine');
+    }
 }
--- app/CTPurchase.php
+++ app/CTPurchase.php
@@ -3,8 +3,54 @@
 namespace Cellar;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CTPurchase extends Model
 {
-    //
+    use SoftDeletes;
+
+    public function user()
+    {
+        return $this->belongsTo('Cellar\User');
+    }
+
+    public function availability()
+    {
+        return $this->hasOne('Cellar\CTPurchase', 'i_wine', 'i_wine');
+    }
+
+    public function consumed()
+    {
+        return $this->hasOne('Cellar\CTPurchase', 'i_wine', 'i_wine');
+    }
+
+    public function inventory()
+    {
+        return $this->hasMany('Cellar\CTPurchase', 'i_wine', 'i_wine');
+    }
+
+    public function list()
+    {
+        return $this->hasOne('Cellar\CTPurchase', 'i_wine', 'i_wine');
+    }
+
+    public function notes()
+    {
+        return $this->hasOne('Cellar\CTPurchase', 'i_wine', 'i_wine');
+    }
+
+    public function pending()
+    {
+        return $this->hasOne('Cellar\CTPurchase', 'i_wine', 'i_wine');
+    }
+
+    public function privateNotes()
+    {
+        return $this->hasOne('Cellar\CTPurchase', 'i_wine', 'i_wine');
+    }
+
+    public function proReview()
+    {
+        return $this->hasOne('Cellar\CTPurchase', 'i_wine', 'i_wine');
+    }
 }
